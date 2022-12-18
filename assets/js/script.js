aa42ff4743047a028b7f2fe70ab6bad7a8bbaf76
nHur2004 <nathan.hur04@gmail.com>
2022-12-17 20:21:45 -0700

    script.js setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+var cities = [];
+
+var searchForm = document.getElementById("searchForm");
+var cityInput = document.getElementById("citySearchInput");
+var pastSearchesContainer = document.getElementById("historyList");
+var cityWeatherInfo = document.getElementById("cityInfo");
+var fiveDayForecast = document.getElementById("forecast");
 const apiKey = "192ae29ad0b5ef84938fe9c3a7e8baed";
