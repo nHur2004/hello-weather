b846db2ae3d30f0f6a309a110a35a431d6f2b5b7
nHur2004 <nathan.hur04@gmail.com>
2022-12-17 20:37:17 -0700

    script.js roadmap

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,17 @@ var pastSearchesContainer = document.getElementById("historyList");
 var cityWeatherInfo = document.getElementById("cityInfo");
 var fiveDayForecast = document.getElementById("forecast");
 const apiKey = "192ae29ad0b5ef84938fe9c3a7e8baed";
+
+// City Search functionality
+
+// City added to historyList
+
+// historyList functionality | click previous city to search that city | put thru weatherCondition function
+
+// searchedCity's weather condition display
+
+// UV index color functionality
+
+// forecast display and functionality
+
+// save and load to localStorage functionality
